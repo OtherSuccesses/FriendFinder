61d483d689e517f0d948dd748a928b8ebe91224b
OtherSuccesses <viskaslietuvai@gmail.com>
2018-02-15 15:07:39 -0500

    Server file actually mostly working.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,10 +1,20 @@
 var friends = require("./../data/friends.js");
 
+var findFriend = function(){
+	var scoreArray = [];
+	var user = 
+}
+
 module.exports = function(app){
 	var user;
 	app.post("/api/friends", function(req, res){
 		module.exports.user = req.body;
 		friends.array.push(req.body);
-		
+		res.json({});
+	});
+
+	app.get("/bestFriend", function(req, res){
+		var bestFriend= findFriend();
+		res.json(bestFriend);
 	})
 }
--- server.js
+++ server.js
@@ -2,13 +2,19 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var path = require("path");
-var htmlRoutes;
-var apiRoutes;
-var friends;
+var htmlRoutes = require("./app/routing/htmlRoutes.js");
+var apiRoutes= require("./app/routing/htmlRoutes.js");
+var friends=require("./app/routing/htmlRoutes.js");
 
 var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-app.use
+app.use(express.static(path.join(__dirname, "./app/public")))
+htmlRoutes(app, path);
+apiRoutes(app);
+
+app.listen(PORT, function(){
+	console.log("APP is listening on " + PORT);
+});
